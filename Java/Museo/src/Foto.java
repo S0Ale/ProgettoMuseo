34d6e8efabf2ff8b759e4227cc98a640a5bd0282
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-03-27 18:17:52 +0100

    Create Foto.java

---
+++ Java/Museo/src/Foto.java
@@ -0,0 +1,30 @@
+import java.sql.Date;
+
+public class Foto extends ContenutoMultimediale{
+
+    private Date data;
+    private String descrizione;
+
+    public Foto(int id, String percorso, Date data, String descrizione) {
+        super(id, percorso);
+        this.setData(data);
+        this.setDescrizione(descrizione);
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+}
