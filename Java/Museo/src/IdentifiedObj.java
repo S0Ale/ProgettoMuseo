3ced8c9d726cc7359d10829fc6e1b4ebd4d68c09
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-03-27 18:17:50 +0100

    Create IdentifiedObj.java

---
+++ Java/Museo/src/IdentifiedObj.java
@@ -0,0 +1,17 @@
+public abstract class IdentifiedObj{
+    private int id;
+
+    public IdentifiedObj(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
