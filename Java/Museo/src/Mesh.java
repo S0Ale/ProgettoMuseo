259c38e475619d0c1fbd302bc1e0cdab9ec226e9
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-03-27 18:17:47 +0100

    Create Mesh.java

---
+++ Java/Museo/src/Mesh.java
@@ -0,0 +1,18 @@
+public class Mesh extends ContenutoMultimediale{
+    private int idNuvolaDiPunti;
+
+    public Mesh(int id, String percorso, int idNuvolaDiPunti) {
+        super(id, percorso);
+        this.setIdNuvolaDiPunti(idNuvolaDiPunti);
+    }
+
+    public int getIdNuvolaDiPunti() {
+        return idNuvolaDiPunti;
+    }
+
+    public void setIdNuvolaDiPunti(int idNuvolaDiPunti) {
+        this.idNuvolaDiPunti = idNuvolaDiPunti;
+    }
+
+
+}
