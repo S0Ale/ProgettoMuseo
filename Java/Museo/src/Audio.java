aee1e9e845baeca77ad1ecbd56aeafbd01607c60
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-03-27 18:17:59 +0100

    Create Audio.java

---
+++ Java/Museo/src/Audio.java
@@ -0,0 +1,24 @@
+public class Audio extends ContenutoMultimediale{
+    private String descrizione;
+    private float durata;
+
+    public Audio(int id, String percorso, String descrizione, float durata) {
+        super(id, percorso);
+        this.descrizione = descrizione;
+        this.durata = durata;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public float getDurata() {
+        return durata;
+    }
+    public void setDurata(float durata) {
+        this.durata = durata;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    
+}
