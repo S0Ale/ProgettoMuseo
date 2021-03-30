1b61074b000a83839fbf42c660d22dd6741b7b9a
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-03-30 09:12:18 +0200

    Connessione col DB

--- Java/Museo/src/dbController.java
+++ Java/Museo/src/dbController.java
@@ -1,3 +1,23 @@
+import java.sql.*;
+
 public class dbController {
-  //  private/* Connection */ String connessione;
+
+
+    public Connection connetti(){
+      Connection connessione = null;
+      String url = "jdbc:mysql://127.0.0.1/museoricoale";
+      String user = "root";
+      String password = "";
+
+      try{
+        connessione = DriverManager.getConnection(url, user, password);
+        if(connessione != null){
+            System.out.println("cazzo sì");
+        }
+      }catch(SQLException e){
+        System.out.println(e);
+      }
+      return connessione;
+    }
+  
 } 
