e2c21d0bde8df3c368d604ceb0e07500e7a8bec3
AndyVale <81222307+AndyVale@users.noreply.github.com>
2021-04-01 19:45:58 +0200

    Connessione con server php

--- Java/Museo/src/dbController.java
+++ Java/Museo/src/dbController.java
@@ -1,23 +1,16 @@
-import java.sql.*;
+/*
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-public class dbController {
-
-
-    public Connection connetti(){
-      Connection connessione = null;
-      String url = "jdbc:mysql://127.0.0.1/museoricoale";
-      String user = "root";
-      String password = "";
-
-      try{
-        connessione = DriverManager.getConnection(url, user, password);
-        if(connessione != null){
-            System.out.println("La connessione è avvenuta con successo");
-        }
-      }catch(SQLException e){
-        System.out.println(e);
-      }
-      return connessione;
-    }
-  
-} 
+public class Main {//poi diventerà il dbcontroller
+    public static void main(String[] args){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost/museo/x.php?prova=1")).build();
+        client.sendAsync(/*gli dico cosa inviare*//*request, *//*indico il formato in cui lo voglio (stringa in questo caso)*//*HttpResponse.BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)//dopo che ricevo l'http "guardo" il body
+        .thenAccept(System.out::println)//e dopo lo printo
+        .join();
+}
+*/
